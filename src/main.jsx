fa228f38da7bb09a330e1dab7f8abe45f9f80b08
François <francois.vittecoq@osteopathe-eso.fr>
2025-08-06 09:29:44 +0200

    create new files for project (jsx and css)

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import "./index.css";
+import Accueil from "./pages/Accueil.jsx";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Router future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+      <Routes>
+        <Route path="/" element={<Accueil />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
---
+++ src/pages/Accueil.jsx
@@ -0,0 +1,8 @@
+export default function Accueil() {
+  return (
+    <div>
+      <h1>Page d'accueil 🏠</h1>
+      <p>Ceci est la page d'accueil</p>
+    </div>
+  );
+}
